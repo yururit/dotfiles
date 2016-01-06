391d3ae8a5aea8479c1c543b03b1cfd0333fe7aa
yururit <c.yururit@gmail.com>
2016-01-06 11:02:08 +0900

    setup項目追加とコメント修正

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
 
@@ -7,11 +10,16 @@ mkdir -p $(anyenv root)/plugins
 git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 git clone https://github.com/znz/anyenv-git.git $(anyenv root)/plugins/anyenv-git
 
-# Perl
+# plenv
 anyenv install plenv
 
-# Ruby
+# rbenv
 anyenv install rbenv
 
-# Node.js
+# ndenv
 anyenv install ndenv
+
+# tmux
+brew install tmux
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
