541c6b92ba5b90f3d01b73ec5f9507c50d6dd05c
yururit <c.yururit@gmail.com>
2018-04-28 23:47:55 +0900

    anyenvがこけるようになってたので暫定対応

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
 
-mkdir -p $(anyenv root)/plugins
+# envsはhttps://github.com/riywo/anyenv/pull/59がマージされるまでの暫定対応
+mkdir -p $(anyenv root)/plugins $(anyenv root)/envs
 git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 git clone https://github.com/znz/anyenv-git.git $(anyenv root)/plugins/anyenv-git
 
